288248ccf1d07854cab652f161937e6d565c3443
emertarion <rvrd1996@gmail.com>
2020-09-03 17:10:17 +0500

    minor changes

---
+++ precache-manifest.0f64280a3f21f750683c769624dec57c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906d6771e3edf2dd689716a1fe13ed20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37fdea450efeb3107ca9",
+    "url": "/static/css/main.84d84768.chunk.css"
+  },
+  {
+    "revision": "138e33c0cc9b1e2dc594",
+    "url": "/static/js/2.bf0d5988.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.bf0d5988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37fdea450efeb3107ca9",
+    "url": "/static/js/main.a4f10e1d.chunk.js"
+  },
+  {
+    "revision": "dfa13489872c10d29614",
+    "url": "/static/js/runtime-main.1d018977.js"
+  },
+  {
+    "revision": "52c3bb2e42e78158ab66111a39ef0c63",
+    "url": "/static/media/Exo2.0-Thin.52c3bb2e.woff"
+  },
+  {
+    "revision": "d8e858b2562dc1350ac143b535683af1",
+    "url": "/static/media/Exo20-Thin.d8e858b2.woff2"
+  },
+  {
+    "revision": "87c2ef8961a4d01cf0d35160047c056c",
+    "url": "/static/media/Photo.87c2ef89.png"
+  },
+  {
+    "revision": "211b74c002e0fb5e2f197166fbd7c006",
+    "url": "/static/media/Shadow.211b74c0.png"
+  }
+]);
